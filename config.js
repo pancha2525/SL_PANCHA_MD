ea8b9eeee04210f302af493b5b9bcac797151410
pancha2525 <alondra20346@gmail.com>
2024-11-27 11:50:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "EtQiHKab#WKhBkKqWeshlRGH3IQA0T0er5N_4tvQDUQKMmgBRIQk",
+MONGODB: process.env.MONGODB || "mongodb://mongo:ANhaXesuaDgitpHJaZkzNoyoxdRnXLSd@junction.proxy.rlwy.net:34193",
 };
 config.js
 Displaying config.js.
