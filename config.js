07c693234c3959498e946be67a2ac2ac4d7163ab
pancha2525 <alondra20346@gmail.com>
2024-12-06 12:18:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "EtQiHKab#WKhBkKqWeshlRGH3IQA0T0er5N_4tvQDUQKMmgBRIQk",
+SESSION_ID: process.env.SESSION_ID || "PypDSaqB#ALg5_QS-mRhUe32-Uwimlb87E4tjctvtuCv4s7Ixwm8",
 MONGODB: process.env.MONGODB || "mongodb://mongo:rMqkSeEtKJNSjhqkKaRSzScJoLoyxulo@junction.proxy.rlwy.net:41051",
 };
